b4aec94b037559af4758eb935239815a38997265
Juuso Saarinen <juuso.saarinen@dqcomms.com>
2020-04-19 14:36:30 +0300

    Add hero image

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -1,3 +1,11 @@
 <div class="c-hero">
-	<h1><?= the_title() ?></h1>
+	<?php while (have_posts()) : the_post(); ?>
+
+	<div class="content">
+		<h1><?= the_title() ?></h1>
+		<?php the_content(); ?>
+	</div>
+	<?= the_post_thumbnail() ?>
+
+	<?php endwhile; ?>
 </div>
