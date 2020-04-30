a69c3444a6177848594e915dd090952c1c759a2f
jsaarine <juuso.saarinen@dqcomms.com>
2020-04-30 09:30:36 +0300

    Remove if from loop

--- index.php
+++ index.php
@@ -13,14 +13,12 @@ get_header();
 			'post_type' => 'post'
 		));
 		
-		if($posts->have_posts()) : while ( $posts->have_posts() ) : $posts->the_post(); ?>
+		while($posts->have_posts()) : $posts->the_post(); ?>
 
 			<h2 style="margin-bottom: 1em;"><?= get_the_title() ?></h2>
 			<?php the_excerpt() ?>
 			<a href="<?= the_permalink() ?>" class="c-button" style="margin-top: 1em;">Read more</a>
-		<?php
-		endwhile; endif;
-		?>
+		<?php endwhile; ?>
 	</div>
 </main>
 
