009c87cab8462d3f06dc3000f0231127d30dcea2
jsaarine <juuso.saarinen@gmail.com>
2020-05-16 16:46:16 +0300

    Hero changes

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -1,11 +1,10 @@
-<div class="c-hero">
-	<?php while (have_posts()) : the_post(); ?>
-
+<div class="c-hero <?= has_post_thumbnail() ? 'm-image' : "m-background" ?>">
 	<div class="content">
 		<h1><?= the_title() ?></h1>
-		<?php the_content(); ?>
+		<p><?= get_post_meta(get_the_ID(), "description", true) ?></p>
+		<?php if(is_front_page()) : ?>
+		<p><a href="">GitHub</a> v1.0.0</p>
+		<?php endif; ?>
 	</div>
 	<?= the_post_thumbnail() ?>
-
-	<?php endwhile; ?>
 </div>
