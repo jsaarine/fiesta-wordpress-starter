55e393eac26499ddb8df9ede120d88599d3bb92c
jsaarine <juuso.saarinen@gmail.com>
2020-08-16 21:08:22 +0300

    Fix hero overflow bug

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -1,10 +1,12 @@
 <div class="c-hero <?= get_query_var('hero_classes') ?>">
 	<div class="content">
-		<h1><?= is_404() ? __('Page not found') : the_title() ?></h1>
-		<p><?= get_post_meta(get_the_ID(), "description", true) ?></p>
-		<?php if(is_front_page()) : ?>
-		<p><a href="https://github.com/jsaarine/fiesta-wordpress-starter">GitHub</a> v1.0.0</p>
-		<?php endif; ?>
+		<div>
+			<h1><?= is_404() ? __('Page not found') : the_title() ?></h1>
+			<p><?= get_post_meta(get_the_ID(), "description", true) ?></p>
+			<?php if(is_front_page()) : ?>
+			<p><a href="https://github.com/jsaarine/fiesta-wordpress-starter">GitHub</a> v<?= wp_get_theme()->get('Version') ?></p>
+			<?php endif; ?>
+		</div>
 	</div>
 	<?= the_post_thumbnail('hero', array('loading' => false)) ?>
 </div>
