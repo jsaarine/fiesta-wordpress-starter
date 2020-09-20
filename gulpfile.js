1740f025f69fdad84529605c53e50dd373dc5217
jsaarine <juuso.saarinen@gmail.com>
2020-09-20 10:38:51 +0300

    Add browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -7,54 +7,66 @@ const sourcemaps = require('gulp-sourcemaps');
 const postcss = require('gulp-postcss');
 const postcssPresetEnv = require('postcss-preset-env');
 const cssnano = require('cssnano');
+const browserSync = require('browser-sync').create();
+const devUrl = "http://localhost:10017";
 
 gulp.task('sass', () => {
-    return gulp.src(['./scss/**/*.scss'])
-        .pipe(sass().on('error', sass.logError))
-        .pipe(postcss([
-            postcssPresetEnv()
-        ]))
-        .pipe(gulp.dest('./dist/css'));
+	return gulp.src(['./scss/**/*.scss'])
+		.pipe(sass().on('error', sass.logError))
+		.pipe(postcss([
+			postcssPresetEnv()
+		]))
+		.pipe(gulp.dest('./dist/css'))
+		.pipe(browserSync.stream());
 });
 
 gulp.task('babel', () => {
-    return gulp.src([
-        './js/vendor/polyfills.js',
-        './lib/js/Core.js',
-        './lib/js/Component.js',
-        './js/Header.js',
-        './js/Navigation.js',
-        './js/App.js',
-    ])
-        .pipe(sourcemaps.init())
-        .pipe(babel({
-            presets: ["@babel/preset-env"],
-            ignore: ['./js/vendor']
-        }))
-        .pipe(concat("./script.js"))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./dist/js'));
+	return gulp.src([
+		'./js/vendor/polyfills.js',
+		'./lib/js/Core.js',
+		'./lib/js/Component.js',
+		'./js/Header.js',
+		'./js/Navigation.js',
+		'./js/App.js',
+	])
+		.pipe(sourcemaps.init())
+		.pipe(babel({
+			presets: ["@babel/preset-env"],
+			ignore: ['./js/vendor']
+		}))
+		.pipe(concat("./script.js"))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('./dist/js'))
+		.pipe(browserSync.stream({match: '**/*.js'}));
 });
 
 gulp.task('watch', () => {
-    gulp.watch(['./scss/**/*.scss', './lib/scss/**/*.scss'], gulp.series('sass'));
-    gulp.watch(['./js/**/*.js', './lib/js/**/*.js'], gulp.series(['babel']));
+	browserSync.init({
+		files: [
+			'./**/*.php',
+		],
+		proxy: devUrl,
+		open: false,
+	});
+
+	gulp.watch(['./scss/**/*.scss', './lib/scss/**/*.scss'], gulp.series('sass'));
+	gulp.watch(['./js/**/*.js', './lib/js/**/*.js'], gulp.series(['babel']));
 });
 
 gulp.task('minify', () => {
-    gulp.src(['./dist/css/style.css'])
-        .pipe(postcss([
-            cssnano(),
-        ]))
-        .pipe(gulp.dest('./dist/css'));
+	gulp.src(['./dist/css/style.css'])
+		.pipe(postcss([
+			cssnano(),
+		]))
+		.pipe(gulp.dest('./dist/css'));
 
-    return gulp.src([
-        './dist/js/script.js',
-    ])
-        .pipe(sourcemaps.init())
-        .pipe(uglify())
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./dist/js'));
+	return gulp.src([
+		'./dist/js/script.js',
+	])
+		.pipe(sourcemaps.init())
+		.pipe(uglify())
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('./dist/js'));
 });
 
 gulp.task('default', gulp.series(['sass', 'babel', 'watch']));
