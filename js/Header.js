d5b14f2d1d3803c5a5f52f6e001232be4f520839
jsaarine <juuso.saarinen@gmail.com>
2020-12-31 09:50:50 +0200

    Replace scroll event with IntersectionObserver

--- js/Header.js
+++ js/Header.js
@@ -2,9 +2,6 @@ class Header {
 
 	constructor(el) {
 		this.el = el;
-		this.oldScroll = 0;
-		this.firstLoad = true;
-		this.threshold = 0;
 
 		this.build();
 	}
@@ -13,41 +10,20 @@ class Header {
 	 * Build the header
 	 */
 	build() {
-		window.addEventListener("scroll", e => {
-			this.update();
-		});
+		const options = {
+			rootMargin: "0px 0px 0px 0px"
+		};
 
-		this.update();
-	}
-
-	/**
-	 * Update the header based on scroll position
-	 */
-	update() {
-		const top = Core.scrollTop();
-
-		// Small (header is visible but smaller when scrolling)
-		if(top > this.threshold) {
-			this.el.classList.add("small");
-		}
-		else {
-			this.el.classList.remove("small");
-		}
-
-		// Hidden (header is hidden when scrolling down)
-		if(Math.abs(top - this.oldScroll) > 0) {
-			if(this.oldScroll > top) {
-				this.el.classList.remove("hidden");
-			}
-			else {
-				if(top > this.threshold && !this.firstLoad) {
-					this.el.classList.add("hidden");
+		const observer = new IntersectionObserver(entries => {
+			entries.forEach(item => {
+				if(item.isIntersecting) {
+					this.el.classList.remove("small");
+				} else {
+					this.el.classList.add("small");
 				}
-			}
+			});
+		}, options);
 
-			this.oldScroll = top;
-		}
-
-		this.firstLoad = false;
+		observer.observe(this.el);
 	}
 }
