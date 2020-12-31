a0369c0b8153285507eea5e51c909ebf0de1a85a
jsaarine <juuso.saarinen@gmail.com>
2020-12-31 09:44:38 +0200

    Hero layout changes

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -2,17 +2,22 @@
 
 namespace Fiesta;
 
+$has_thumbnail = has_post_thumbnail();
+$description = get_post_meta(get_the_ID(), "description", true);
+
 ?>
 
-<div class="c-hero <?= $args['hero_classes'] ?>">
-	<div class="content">
+<div class="c-hero <?= $args['hero_classes'] ?><?= $has_thumbnail ? ' image' : 'plain' ?>">
+	<?= the_post_thumbnail('hero', array('loading' => false)) ?>
+	<div class="text">
 		<div>
 			<h1><?= is_404() ? __('Page not found') : the_title() ?></h1>
-			<p><?= get_post_meta(get_the_ID(), "description", true) ?></p>
+			<?php if($description) : ?>
+				<p><?= $description ?></p>
+			<?php endif; ?>
 			<?php if(is_front_page()) : ?>
-			<p><a href="https://github.com/jsaarine/fiesta-wordpress-starter">GitHub</a> v<?= wp_get_theme()->get('Version') ?></p>
+				<p><a href="https://github.com/jsaarine/fiesta-wordpress-starter">GitHub</a> v<?= wp_get_theme()->get('Version') ?></p>
 			<?php endif; ?>
 		</div>
 	</div>
-	<?= the_post_thumbnail('hero', array('loading' => false)) ?>
 </div>
