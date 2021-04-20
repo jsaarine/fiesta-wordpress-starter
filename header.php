f0c9462cb9cb76f83112afd272b5028c4ad64f9a
jsaarine <juuso.saarinen@gmail.com>
2021-04-20 21:54:07 +0300

    Remove JavaScript breakpoints feature

--- header.php
+++ header.php
@@ -7,7 +7,6 @@ namespace Fiesta;
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 	<head>
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta charset="<?php bloginfo('charset'); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
 		<?php wp_head(); ?>
