87170aa0a97c4dcff901984c4b592be2eda0c192
jsaarine <juuso.saarinen@gmail.com>
2021-08-02 14:23:42 +0300

    Minor fixes

--- template-parts/page-header.php
+++ template-parts/page-header.php
@@ -8,9 +8,6 @@ namespace Fiesta;
 	<div class="text">
 		<div>
 			<h1><?= is_404() ? __('Page not found') : the_title() ?></h1>
-			<?php if($description) : ?>
-				<p><?= $description ?></p>
-			<?php endif; ?>
 		</div>
 	</div>
 </div>
