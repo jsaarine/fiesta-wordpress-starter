5718bbd566494005fab7abbb43eb7f162d294c2f
jsaarine <juuso.saarinen@gmail.com>
2021-08-30 17:27:59 +0300

    Fix 404 page typo

--- 404.php
+++ 404.php
@@ -5,7 +5,7 @@ namespace Fiesta;
 get_header(); ?>
 
 <main id="main-content" tabindex="-1">
-	<?php get_template_part('template-parts/page_header'); ?>
+	<?php get_template_part('template-parts/page-header'); ?>
 
 	<div class="c-article">
 
