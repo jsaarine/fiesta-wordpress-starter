b901866552f3a0268906e1a4a7c346722b93c46c
jsaarine <juuso.saarinen@gmail.com>
2021-09-20 08:17:54 +0300

    Fix allowed_block_types_all error

--- inc/blocks.php
+++ inc/blocks.php
@@ -4,33 +4,30 @@ namespace Fiesta;
 
 /* Allowed Block types */
 
-add_filter('allowed_block_types_all', function($allowed_block_types, $post) {
-	switch($post->post_type):
-		default:
-			return array(
-				'core/block',
-				'core/audio',
-				'core/button',
-				'core/buttons',
-				'core/code',
-				'core/columns',
-				'core/cover',
-				'core/embed',
-				'core/file',
-				'core/gallery',
-				'core/group',
-				'core/heading',
-				'core/image',
-				'core/list',
-				'core/media-text',
-				'core/paragraph',
-				'core/pullquote',
-				'core/quote',
-				'core/separator',
-				'core/shortcode',
-				'core/social-links',
-				'core/table',
-				'core/video',
-			);
-	endswitch;
+add_filter('allowed_block_types_all', function($allowed_block_types, $block_editor_context) {
+	return array(
+		'core/block',
+		'core/audio',
+		'core/button',
+		'core/buttons',
+		'core/code',
+		'core/columns',
+		'core/cover',
+		'core/embed',
+		'core/file',
+		'core/gallery',
+		'core/group',
+		'core/heading',
+		'core/image',
+		'core/list',
+		'core/media-text',
+		'core/paragraph',
+		'core/pullquote',
+		'core/quote',
+		'core/separator',
+		'core/shortcode',
+		'core/social-links',
+		'core/table',
+		'core/video',
+	);
 }, 10, 2);
