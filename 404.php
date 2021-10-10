50e4835114c89af1adb005569c970f1334da76bc
jsaarine <juuso.saarinen@gmail.com>
2021-10-10 22:06:59 +0300

    Fix 404 page content

--- 404.php
+++ 404.php
@@ -7,8 +7,8 @@ get_header(); ?>
 <main id="main-content" tabindex="-1">
 	<?php get_template_part('template-parts/page-header'); ?>
 
-	<div class="c-article">
-
+	<div class="c-article" style="text-align: center;">
+		<p>The page you were looking for was not found.</p>
 	</div>
 </main>
 
