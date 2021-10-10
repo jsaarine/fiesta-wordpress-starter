0ce512f1de8b32b2d7cf9d013a81ee0ffa47b31e
jsaarine <juuso.saarinen@gmail.com>
2021-10-10 22:29:09 +0300

    Various small changes

--- template-parts/page-header.php
+++ template-parts/page-header.php
@@ -5,9 +5,5 @@ namespace Fiesta;
 ?>
 
 <div class="c-page-header">
-	<div class="text">
-		<div>
-			<h1><?= is_404() ? __('Page not found') : the_title() ?></h1>
-		</div>
-	</div>
+	<h1><?= is_404() ? __('Page not found') : the_title() ?></h1>
 </div>
