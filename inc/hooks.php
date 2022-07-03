aa18a8b22ed9f0083250e91d6b8fb71627ad5e14
jsaarine <juuso.saarinen@gmail.com>
2022-07-03 19:15:10 +0300

    Remove has-featured-image body class

--- inc/hooks.php
+++ inc/hooks.php
@@ -1,16 +1,3 @@
 <?php
 
 namespace Fiesta;
-
-
-/* Body class */
-
-add_filter('body_class', function($classes) {
-	global $post;
-
-	if(isset($post->ID) && get_the_post_thumbnail($post->ID)) {
-		$classes[] = 'has-featured-image';
-	}
-
-	return $classes;
-});
