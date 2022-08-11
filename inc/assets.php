6c314ab874c8feddfb0c6d200156dc0498db6d14
jsaarine <juuso.saarinen@gmail.com>
2022-08-11 15:58:10 +0300

    Add version to vendor js

--- inc/assets.php
+++ inc/assets.php
@@ -9,7 +9,7 @@ add_action('wp_enqueue_scripts', function() {
 	$theme_version = wp_get_theme()->get('Version');
 
 	wp_enqueue_style(PREFIX, get_stylesheet_directory_uri().'/dist/css/style.css', [], $theme_version);
-	wp_enqueue_script(PREFIX.'-vendor', get_stylesheet_directory_uri().'/js/vendor/vendor.js', [], null, true);
+	wp_enqueue_script(PREFIX.'-vendor', get_stylesheet_directory_uri().'/js/vendor/vendor.js', [], $theme_version, true);
 	wp_enqueue_script(PREFIX, get_stylesheet_directory_uri().'/dist/js/script.js', [], $theme_version, true);
 	wp_localize_script(PREFIX, 'themeVariables', ['themefolder' => get_stylesheet_directory_uri()]);
 });
