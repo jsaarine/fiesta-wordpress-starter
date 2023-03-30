288ee1e638f6a1421e0550ca101f09bba85c15aa
jsaarine <juuso.saarinen@gmail.com>
2023-03-30 08:22:05 +0300

    Add header and footer as template parts

--- footer.php
+++ footer.php
@@ -4,10 +4,8 @@ namespace Fiesta;
 
 ?>
 
-<footer class="c-footer">
-	<div class="container">
-		<?php block_template_part('footer'); ?>
-	</div>
+<footer class="c-article">
+	<?php block_template_part('footer'); ?>
 </footer>
 
 <?php wp_footer();
