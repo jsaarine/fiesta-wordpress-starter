302eae6ac849d718447bc759d8ed79f1b04dc6b9
jsaarine <juuso.saarinen@gmail.com>
2023-07-26 16:06:51 +0300

    Add single template

---
+++ single.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Fiesta;
+
+get_header();
+
+?>
+
+<main id="main-content">
+	<?php while(have_posts()) : the_post(); ?>
+
+		<article class="c-article">
+			<h1><?php the_title() ?></h1>
+			<?php the_content() ?>
+		</article>
+
+		<?php get_template_part('template-parts/share'); ?>
+
+	<?php endwhile; ?>
+</main>
+
+<?php get_footer();
