a5f6bd22b72bfe05eadf67f1861736d8a7e50f8a
jsaarine <juuso.saarinen@gmail.com>
2023-07-27 13:57:38 +0300

    Add gallery block

--- js/editor/editor.js
+++ js/editor/editor.js
@@ -1,5 +1,5 @@
 wp.domReady(function() {
-	// Remove editor styles
+	// Remove block styles
 	wp.blocks.unregisterBlockStyle('core/image', 'rounded');
 	wp.blocks.unregisterBlockStyle('core/quote', 'large');
 	wp.blocks.unregisterBlockStyle('core/quote', 'plain');
@@ -8,42 +8,10 @@ wp.domReady(function() {
 	wp.blocks.unregisterBlockStyle('core/separator', 'wide');
 	wp.blocks.unregisterBlockStyle('core/table', 'stripes');
 	wp.blocks.unregisterBlockStyle('core/social-links', 'pill-shape');
+
+	// Add block styles
+	wp.blocks.registerBlockStyle('core/button', {
+		name: "dark",
+		label: "Dark"
+	});
 });
-
-// Filter block alignment options
-wp.hooks.addFilter(
-	'blocks.registerBlockType',
-	'fiesta/block-alignment',
-	function(settings, name) {
-		var align;
-		var defaultAlign;
-
-		switch(name) {
-			case 'core/gallery':
-				align = ['wide', 'full'];
-				defaultAlign = 'full';
-			break;
-			case 'core/columns':
-				align = ['wide', 'full'];
-				defaultAlign = 'wide';
-			break;
-			case 'core/cover':
-				align = ['wide', 'full'];
-				defaultAlign = 'full';
-			break;
-			default:
-				return settings;
-		}
-
-		return lodash.assign({}, settings, {
-			supports: lodash.assign({}, settings.supports, {
-				align: align,
-			}),
-			attributes: lodash.assign({}, settings.attributes, {
-				align: lodash.assign({}, settings.attributes.align, {
-					default: defaultAlign,
-				})
-			}),
-		});
-	}
-);
