1c1f98e9f197d067240fd85b2ba831becf06045e
jsaarine <juuso.saarinen@gmail.com>
2023-07-28 09:59:29 +0300

    Add favicon

--- inc/assets.php
+++ inc/assets.php
@@ -20,3 +20,20 @@ add_action('enqueue_block_editor_assets', function() {
 
 	wp_enqueue_script(PREFIX.'-editor', get_stylesheet_directory_uri() . '/js/editor/editor.js', ['wp-blocks', 'wp-dom'], $theme_version, true);
 });
+
+// Favicon
+function add_favicon() {
+	echo '
+		<link rel="apple-touch-icon" sizes="180x180" href="'.get_stylesheet_directory_uri().'/images/favicon/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="'.get_stylesheet_directory_uri().'/images/favicon/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="'.get_stylesheet_directory_uri().'/images/favicon/favicon-16x16.png">
+		<link rel="icon" type="image/svg+xml" href="'.get_stylesheet_directory_uri().'/images/favicon/favicon.svg">
+		<link rel="manifest" href="'.get_stylesheet_directory_uri().'/images/favicon/site.webmanifest">
+		<link rel="mask-icon" href="'.get_stylesheet_directory_uri().'/images/favicon/safari-pinned-tab.svg" color="#694cd4">
+		<meta name="theme-color" content="#ffffff">
+	';
+}
+
+add_action('wp_head', __NAMESPACE__.'\add_favicon');
+add_action('admin_head', __NAMESPACE__.'\add_favicon');
+add_action('login_head', __NAMESPACE__.'\add_favicon');
