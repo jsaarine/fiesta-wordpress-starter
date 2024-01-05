bab5442634a88530adda8409329381ed53052175
jsaarine <juuso.saarinen@gmail.com>
2024-01-05 12:43:33 +0200

    Add translate private cpt and tax sample code

--- inc/translations.php
+++ inc/translations.php
@@ -8,3 +8,26 @@ if(function_exists('pll_register_string')) {
 	// Polylang translations here
 	// pll_register_string(PREFIX, 'Example translation');
 }
+
+
+/* Translate private post types and taxonomies */
+
+/*add_filter('pll_get_post_types', function ($post_types, $is_settings) {
+	if($is_settings) {
+		unset($post_types['cpt']);
+	} else {
+		$post_types['cpt'] = 'cpt';
+	}
+
+	return $post_types;
+}, 10, 2);
+
+add_filter('pll_get_taxonomies', function ($taxonomies, $is_settings) {
+	if($is_settings) {
+		unset($taxonomies['taxonomy']);
+	} else {
+		$taxonomies['taxonomy'] = 'taxonomy';
+	}
+
+	return $taxonomies;
+}, 10, 2);*/
